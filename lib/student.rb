3df525ecd9bab6dc03d9d15ba4ad1ead09d1225d
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-23 07:03:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,16 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
-class Student < InteractiveRecord
+class Student < InteractiveRecord 
+  
+ self.column_names.each do |attributes| 
+   attr_accessor attributes.to_sym
+ end 
+ 
+ def initialize(options = {})  
+   options.each do |key,val| 
+     self.send("#{key}=", val)
+   end
+ end
 
 end
